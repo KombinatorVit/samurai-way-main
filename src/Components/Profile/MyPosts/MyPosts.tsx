f7ff1f40ace4330adbf58846e6b94b0bf174d5b3
Vit Onischenko <slonik0775@gmail.com>
2022-07-27 01:01:07 +0300

    onClick, ref, VirtualDOM

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -9,16 +9,22 @@ const MyPosts = (props:any) => {
     let postsElements = props.posts.map((p:any) => <Post message={p.message} likesCount={p.likesCount}/>);
 
 
+let newPostElement: any  = React.createRef()
+
+    const addPostButtonHandler = () => {
+let text = newPostElement.current.value
+alert(text)
+}
     return (
         <div className={s.postsBlock}>
             <h3> My posts
             </h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPostButtonHandler}>Add post</button>
                     <button>Remove</button>
 
                 </div>
