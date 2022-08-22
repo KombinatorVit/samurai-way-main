274e54e4f039df876d0dbcfb38adc3d298b8297a
Vit Onischenko <slonik0775@gmail.com>
2022-08-22 23:57:35 +0300

    71: status: setState

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from './ProfileStatus';
 
 
 const ProfileInfo = (props:any) => {
@@ -8,11 +9,12 @@ const ProfileInfo = (props:any) => {
         return <Preloader />
     }
     return (<div>
-    <div>
-      <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' alt='img' />
-    </div>
+    {/*<div>*/}
+    {/*  <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' alt='img' />*/}
+    {/*</div>*/}
     <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large} />
+        <ProfileStatus status={"Hello my friends"}/>
       <div><span>{props.profile.fullName}</span></div>
       <div><span>{props.profile.lookingForAJobDescription}</span></div>
       <div><span>{props.profile.contacts.github}</span></div>
