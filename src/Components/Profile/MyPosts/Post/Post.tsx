48cba8c46a3faeec39364576763c2ac43db51ba4
Vit Onischenko <slonik0775@gmail.com>
2022-09-01 01:13:22 +0300

    update avatar

--- src/Components/Profile/MyPosts/Post/Post.tsx
+++ src/Components/Profile/MyPosts/Post/Post.tsx
@@ -9,7 +9,7 @@ type PropsType = {
 const Post: React.FC<PropsType> = (props) => {
     return (
         <div className={s.item}>
-            <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg' />
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5faVszR5XQKwYrgak66d8QDijo5m739aLfQ&usqp=CAU' />
             { props.message }
             <div>
                 <span>like</span> { props.likesCount }
